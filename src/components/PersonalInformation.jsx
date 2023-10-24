a8ce2ed965fbd857a4b07f0506ab90eb98166cb0
Karol <kpulawski13@gmail.com>
2023-10-24 10:42:36 +0100

    Created PersonalInformationSection

---
+++ src/components/PersonalInformation.jsx
@@ -0,0 +1,30 @@
+import Section from './Section';
+
+function ContactDetails() {
+  return (
+    <div>
+      <p>Somewhere in town, 55O-OXX</p>
+      <p>
+        <span>
+          <b>M:</b> 555-555-555{' '}
+        </span>
+        <span>
+          <b>E:</b> kpulawski13@gmail.com
+        </span>
+      </p>
+    </div>
+  );
+}
+
+function PersonalInformationSection() {
+  return (
+    <>
+      <Section>
+        <h1>Name Surname</h1>
+        <ContactDetails />
+      </Section>
+    </>
+  );
+}
+
+export default PersonalInformationSection;
