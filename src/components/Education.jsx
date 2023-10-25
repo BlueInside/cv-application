a532689eac8c8683a26132ba2934d648b62a169b
Karol <kpulawski13@gmail.com>
2023-10-25 12:25:52 +0100

    Implement EducationSection component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,22 +1,44 @@
 import Section from './Section';
+import Button from './Button';
+import { useState } from 'react';
 
-function School() {
+let count = 0;
+const educationData = [
+  {
+    id: count++,
+    schoolName: 'Secondary school named "Tadeusza Kosciuszki" Poland',
+    title: 'Matriculation Certificate',
+    date: 'June 2015',
+    description:
+      'Completed secondary education with a focus on mathematics and geography, achieving the Matriculation Certificate, equivalent to a high school diploma in Poland.',
+  },
+];
+
+function School({ education }) {
+  function handleClick(e) {}
   return (
     <>
-      <p>School Name</p>
-      <p>
-        Title of Study <span>Date</span>
-      </p>
-      <p>Description</p>
+      <div>
+        <p>{education.schoolName}</p>
+        <p>{education.title}</p>
+      </div>
+      <div>
+        <p>{education.description} </p>
+        <p>{education.date}</p>
+      </div>
+      <Button text={'edit'} />
     </>
   );
 }
 
 function EducationSection() {
+  const [education, newEdutacion] = useState(educationData);
   return (
     <Section>
       <h2>Education</h2>
-      <School />
+      {education.map((education) => (
+        <School key={education.id} education={education} />
+      ))}
     </Section>
   );
 }
