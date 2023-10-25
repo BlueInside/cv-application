96d87f303a5062915ad011a5aabd56ea01e48539
Karol <kpulawski13@gmail.com>
2023-10-25 08:12:37 +0100

    Introduce edit mode for user details in CVHeader

--- src/components/CVHeader.jsx
+++ src/components/CVHeader.jsx
@@ -3,23 +3,44 @@ import { useState } from 'react';
 import Button from './Button';
 
 const inputFields = [
-  { id: 'name' },
-  { id: 'lastName' },
-  { id: 'email' },
-  { id: 'mobile' },
+  { value: 'name' },
+  { value: 'surname' },
+  { value: 'mobile' },
+  { value: 'email' },
+  { value: 'address' },
+  { value: 'county' },
+  { value: 'postCode' },
 ];
-function ContactDetails() {
+function ContactDetails({ user }) {
   return (
     <div>
-      <p>Somewhere in town, 55O-OXX</p>
+      <p>
+        {user.address}, {user.county !== '' && user.county + ','}{' '}
+        {user.postCode}
+      </p>
       <p>
         <span>
-          <b>M:</b> 555-555-555{' '}
+          <b>M:</b> {user.mobile}{' '}
         </span>
         <span>
-          <b>E:</b> kpulawski13@gmail.com
+          <b>E:</b> {user.email}
         </span>
       </p>
+    </div>
+  );
+}
+
+function EditUserInfoFields({ user, handleInputChange }) {
+  return (
+    <div>
+      {inputFields.map((input) => (
+        <input
+          key={input.index}
+          placeholder={input.value}
+          value={user[input.value]}
+          onChange={(e) => handleInputChange(e, input.value)}
+        />
+      ))}
     </div>
   );
 }
@@ -28,9 +49,12 @@ function CVHeader() {
   const [isEditing, setIsEditing] = useState(false);
   const [user, setUser] = useState({
     name: 'Karol',
-    lastName: 'Pulawski',
+    surname: 'Pulawski',
     mobile: '555-555-555',
     email: 'kpulawski13@gmail.com',
+    address: 'Somewhere in Tow',
+    county: 'Townshire',
+    postCode: '555-ZZZ',
   });
 
   function handleClick() {
@@ -46,21 +70,23 @@ function CVHeader() {
     <>
       <Section>
         {isEditing && (
-          <div>
-            {inputFields.map((input) => (
-              <input
-                key={input.id}
-                value={user[input.id]}
-                onChange={(e) => handleInputChange(e, input.id)}
-              />
-            ))}
-          </div>
+          <EditUserInfoFields
+            user={user}
+            handleInputChange={handleInputChange}
+          />
         )}
 
-        <h1> {user.name + ' ' + user.lastName} </h1>
-        <ContactDetails isEditing={isEditing} />
+        <h1> {user.name + ' ' + user.surname} </h1>
+        <ContactDetails
+          user={{
+            mobile: user.mobile,
+            email: user.email,
+            address: user.address,
+            postCode: user.postCode,
+            county: user.county,
+          }}
+        />
         <Button handleClick={handleClick} color="blue" text={'Edit'} />
-        {isEditing && <Button color="red" text={'Submit'} />}
       </Section>
     </>
   );
