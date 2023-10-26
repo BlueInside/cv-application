b724693ec8a5a867c584e1b69c42afda21d0c9aa
Karol <kpulawski13@gmail.com>
2023-10-26 19:43:35 +0100

    create data file

---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+const educationData = [
+  {
+    id: 0,
+    schoolName: 'Secondary school named "Tadeusza Kosciuszki" Poland',
+    title: 'Matriculation Certificate',
+    date: 'June 2015',
+    description:
+      'Completed secondary education with a focus on mathematics and geography, achieving the Matriculation Certificate, equivalent to a high school diploma in Poland.',
+  },
+];
+
+export { educationData };
