fc3c016f0ff8011a7a9d59162bd467bf126fb466
Karol <kpulawski13@gmail.com>
2023-10-29 19:31:06 +0000

    move worksData

--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,4 @@
+// Data with all the educations
 const educationData = [
   {
     id: 0,
@@ -9,4 +10,21 @@ const educationData = [
   },
 ];
 
-export { educationData };
+// Data with all the jobs
+const worksData = [
+  {
+    id: 0,
+    companyName: 'some company',
+    position: 'janitor',
+    startDate: '2023-05-18',
+    endDate: '2023-10-23',
+    title: 'quick-mop',
+    responsibilities: [
+      'first responsibility',
+      'second responsibility',
+      'third responsibility',
+      'fourth responsibility',
+    ],
+  },
+];
+export { educationData, worksData };
